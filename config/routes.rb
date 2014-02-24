9cacc05bb00d1ddcf913116a198bf761a0091a5b
sergei <muzyukins@gmail.com>
2014-02-24 14:57:45 +0200

    Event Index added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Hari::Application.routes.draw do
   root 'store#index'
-  
+  get 'event' => 'event#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
